b25b9b94f5d6b81b310307d19ce8700980e18c7e
leandro30mz <leandro7moreira30@gmail.com>
2025-12-27 09:18:28 -0500

    comir2

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,108 @@
-import 'package:app_nueva/screens/GuardarScreen.dart';
-import 'package:app_nueva/screens/LeerScreens.dart';
+
+
+import 'package:app_nueva/firebase_options.dart';
 import 'package:app_nueva/screens/LoginScreen.dart';
 import 'package:app_nueva/screens/RegistroScreen.dart';
-import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
 );
-runApp(const MiAplicacion());
+runApp(const MiApp());
 }
 
-class MiAplicacion extends StatelessWidget {
-  const MiAplicacion({super.key});
+
+
+class MiApp extends StatelessWidget {
+  const MiApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/login': (context) => Loginscreen(),
-        '/registro': (context) => Registroscreen(),
-        '/guardar': (context) => Guardarscreen(),
-        '/leer': (context) => LeerScreens(),
-      },
+    return  MaterialApp(
       home: Cuerpo(),
     );
   }
 }
 
-class Cuerpo extends StatefulWidget {
+class Cuerpo extends StatelessWidget {
   const Cuerpo({super.key});
-
-  @override
-  State<Cuerpo> createState() => _CuerpoState();
-}
-
-class _CuerpoState extends State<Cuerpo> {
-  int indice= 0;
-  List<Widget>paginas=[Loginscreen(),Registroscreen(),Guardarscreen(),LeerScreens()];
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("PRINCIPAL")),
-      body: paginas[indice],
-      
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: indice,
-        onTap: (value) {
-          setState(() {
-            indice = value;
-          });
-        },
-
-
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.login), label: "Login"),
-          BottomNavigationBarItem(icon: Icon(Icons.app_registration), label: "Registro"),
-          BottomNavigationBarItem(icon: Icon(Icons.save), label: "Guardar"),
-          BottomNavigationBarItem(icon: Icon(Icons.line_style_sharp), label: "leer"),
-        ]),
-      /*
-      drawer: Drawer(
-
-        child: ListView(
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              title: Text("Login"),
-              onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Loginscreen()),
-              ),
-            ),
-            ListTile(
-              title: Text("Registro"),
-              onTap: () => Navigator.pushNamed(context, '/registro'),
-            ),
-          ],
-        ),
-      ),
-    */
+      appBar: AppBar(title: Text("APP GASTRONOMICA"),
+      actions: [IconButton(onPressed: ()=>(), icon:Icon(Icons.rocket_launch))],),
+     body:botones(context),
     );
   }
 }
+
+ Widget botones(context){
+return Column(children: [
+  
+  
+   FilledButton(onPressed: ()=>irventana1(context), child: Text("desplegar informacion")),
+   
+     ElevatedButton(onPressed: ()=>Navigator.push(context,MaterialPageRoute(builder: (context)=>Loginscreen(),)), child: Text("LOGIN")),
+   
+    ElevatedButton(onPressed: ()=>Navigator.push(context,MaterialPageRoute(builder: (context)=>Registroscreen(),)), child: Text("REGISTRATE")),
+  
+    
+
+
+
+    
+ 
+],);
+
+ }
+
+ void irventana1(context){
+  mostrarInfoModal(context);
+ }
+
+ void mostrarInfoModal(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+       
+        title: Center( 
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Icon(Icons.person, color: Colors.blue),
+              SizedBox(width: 8),
+              Text('Información del Estudiante'),
+            ],
+          ),
+        ),
+        content: const Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Nombre:Leandro Moreirs', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text('Docente:Ing.Julio Rosero'),
+            Text('Carrera:Desarrollo de Software'),
+            Text('Fecha: 06/12/2025'),
+          ],
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Cerrar'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+
+
